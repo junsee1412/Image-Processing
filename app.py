2121ec758afa406986d28fc45ae39aefb52223c2
junsee <dat10319@gmail.com>
2022-04-21 20:45:12 +0700

    add gamma correction :v

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from PyQt5.QtCore import QEvent, QSize, Qt
+from PyQt5.QtCore import QEvent, Qt
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGraphicsPixmapItem, QGraphicsScene, QFileDialog, qApp
-from matplotlib import scale
 from gui.maingui import Ui_MainWindow
-import sys, os, cv2, imutils
-import numpy as np
+from process import Process
+import sys, os, cv2
 
 class MainMeow(QWidget):
 
@@ -17,7 +16,8 @@ class MainMeow(QWidget):
         self.mwg.actionOpen.triggered.connect(self.loadImage)
         self.mwg.actionSave.triggered.connect(self.savePhoto)
         self.mwg.horizontalSlider.valueChanged['int'].connect(self.brightness_value)
-        self.mwg.horizontalSlider_2.valueChanged['int'].connect(self.blur_value)
+        self.mwg.horizontalSlider_2.valueChanged['int'].connect(self.gamma_value)
+        self.mwg.horizontalSlider_3.valueChanged['int'].connect(self.blur_value)
         self.mwg.actionZoom_In.triggered.connect(self.on_zoom_in)
         self.mwg.actionZoom_Out.triggered.connect(self.on_zoom_out)
 
@@ -25,41 +25,49 @@ class MainMeow(QWidget):
 
         self.mwg.actionQuit.triggered.connect(qApp.quit)
         
+        self.process = Process()
         self.path = None
         self.tmp = None
-        self.brightness_value_now = 0
-        self.blur_value_now = 0
+        self.reset()
         self.directory = os.path.expanduser("~")
     
     def eventFilter(self, source, event):
-        if (source == self.mwg.graphicsView.viewport() and 
+        if (
+            # source == self.mwg.graphicsView.viewport() and 
             event.type() == QEvent.Wheel and
             event.modifiers() == Qt.ControlModifier):
                 if event.angleDelta().y() > 0:
                     scale = 1.25
                 else:
-                    scale = .8
+                    scale = 0.8
                 self.mwg.graphicsView.scale(scale, scale)
-                # do not propagate the event to the scroll area scrollbars
+
                 return True
-        elif (event.type() == QEvent.GraphicsSceneMousePress):
-            pass
+        # elif (event.type() == QEvent.GraphicsSceneMousePress):
+        #     pass
             # ...
-        return super().eventFilter(source,event)
+        return super().eventFilter(source, event)
+    
+    def reset(self):
+        self.brightness_value_now = 0
+        self.blur_value_now = 0
+        self.gamma_value_now = 10
+        self.mwg.horizontalSlider.setValue(self.brightness_value_now)
+        self.mwg.horizontalSlider_2.setValue(self.gamma_value_now)
+        self.mwg.horizontalSlider_3.setValue(self.blur_value_now)
 
     def loadImage(self):
         path = QFileDialog.getOpenFileName(self, "Open Image", self.directory, filter="Image (*.*)")[0]
         if (path):
             self.path = path
-            self.scale = 1
-            self.brightness_value_now = 0
-            self.blur_value_now = 0
+            self.reset()
             self.image = cv2.imread(self.path)
             self.setPhoto(self.image)
+            name = os.path.basename(self.path)
+            self.mwg.statusbar.showMessage(name)
 
-    def setPhoto(self,image):
+    def setPhoto(self, image):
         self.tmp = image
-        image = imutils.resize(image,width=640)
         frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = QImage(frame, frame.shape[1],frame.shape[0],frame.strides[0],QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(image)
@@ -79,20 +87,10 @@ class MainMeow(QWidget):
             self.blur_value_now = value
             self.update()
     
-    def changeBrightness(self, img, value):
-        hsv =cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        h,s,v = cv2.split(hsv)
-        lim = 255 - value
-        v[v>lim] = 255
-        v[v<=lim] += value
-        final_hsv = cv2.merge((h,s,v))
-        img = cv2.cvtColor(final_hsv,cv2.COLOR_HSV2BGR)
-        return img
-    
-    def changeBlur(self, img, value):
-        kernel_size = (value + 1, value + 1)
-        img = cv2.blur(img, kernel_size)
-        return img
+    def gamma_value(self, value):
+        if (self.path):
+            self.gamma_value_now = value
+            self.update()
 
     def on_zoom_in(self):
         if (self.path):
@@ -105,8 +103,9 @@ class MainMeow(QWidget):
             self.mwg.graphicsView.scale(scale, scale)
     
     def update(self):
-        img = self.changeBrightness(self.image, self.brightness_value_now)
-        img = self.changeBlur(img, self.blur_value_now)
+        img = self.process.changeBrightness(self.image, self.brightness_value_now)
+        img = self.process.gamma(img, self.gamma_value_now)
+        img = self.process.changeBlur(img, self.blur_value_now)
         self.setPhoto(img)
 
     def savePhoto(self):
@@ -114,10 +113,11 @@ class MainMeow(QWidget):
         if (filename[0]):
             self.path = ''.join(filename)
             cv2.imwrite(self.path,self.tmp)
+            name = os.path.basename(self.path)
+            self.mwg.statusbar.showMessage(name)
 
     def show(self):
         self.main_win.show()
-
 
 if __name__ == "__main__":
     app = QApplication([])
---
+++ process.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+class Process:
+    def changeBrightness(self, img, value):
+        hsv =cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h,s,v = cv2.split(hsv)
+        lim = 255 - value
+        v[v>lim] = 255
+        v[v<=lim] += value
+        final_hsv = cv2.merge((h,s,v))
+        img = cv2.cvtColor(final_hsv,cv2.COLOR_HSV2BGR)
+        return img
+    
+    def changeBlur(self, img, value):
+        kernel_size = (value + 1, value + 1)
+        img = cv2.blur(img, kernel_size)
+        return img
+
+    def gamma(self, img, value):
+        value = value*0.1
+        invGamma = 1.0 /value
+        table = np.array([((i / 255.0) ** invGamma) * 255
+            for i in np.arange(0, 256)]).astype("uint8")
+
+        img = cv2.LUT(img, table)
+        return img
